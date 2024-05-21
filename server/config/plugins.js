fc16c9e783a2a8d3a23ac8f825f138c173a93e6b
Christian Olofsson <ch.olofsson@protonmail.com>
2024-05-21 13:23:29 +0200

    Changed richtext to markdown

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = {
+'strapi-plugin-markdown': {
+    enabled: true,
+    resolve: './node_modules/strapi-plugin-markdown'
+},
+};
+  
+
+// module.exports = () => ({});
