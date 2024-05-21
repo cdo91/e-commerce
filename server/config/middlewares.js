062595403016cdb619d70272a92207a79cc4d7c1
Christian Olofsson <ch.olofsson@protonmail.com>
2024-05-21 16:48:27 +0200

    changed middleware

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,8 +1,31 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+          'script-src': ["'self'", 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+          'style-src': ["'self'", 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+          'connect-src': ["'self'", 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://worthy-bloom-f4d3aff7df.strapiapp.com', 'https://worthy-bloom-f4d3aff7df.strapiapp.com'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+      keepHeadersOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
